4e632e55cfbffbc5085a1faae3525b2755b0fe12
Snigga <stefan.solbeck@gmail.com>
2022-11-06 16:35:22 +0100

    Update

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'google-oauth adapter settings': {
+        'en': 'Adapter settings for google-oauth',
+        'de': 'Adaptereinstellungen für google-oauth',
+        'ru': 'Настройки адаптера для google-oauth',
+        'pt': 'Configurações do adaptador para google-oauth',
+        'nl': 'Adapterinstellingen voor google-oauth',
+        'fr': "Paramètres d'adaptateur pour google-oauth",
+        'it': "Impostazioni dell'adattatore per google-oauth",
+        'es': 'Ajustes del adaptador para google-oauth',
+        'pl': 'Ustawienia adaptera dla google-oauth',
+        'zh-cn': 'google-oauth的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
